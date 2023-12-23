e5876e6d949fd6a45569fe444a1b3b92075d639d
“nicm42” <42087659+nicm42@users.noreply.github.com>
2023-12-23 17:14:57 +0000

    Added heading and subtitle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import Card from './components/Card';
 import './App.css';
 import doctorwho from '../doctor-who.json';
 //import thebill from '../the-bill.json'
 
 function App() {
+  const [whatToShow, setWhatToShow] = useState('');
+  const textToShowSame = whatToShow === 'doctorwho' ? 'Doctor Who' : whatToShow === 'thebill' ? 'The Bill' : '';
+  const textToShowOpposite = whatToShow === 'doctorwho' ? 'The Bill' : whatToShow === 'thebill' ? 'Doctor Who' : '';
+
+  const headerText = `${textToShowSame} regulars who have been in ${textToShowOpposite}`;
+  const subtitleText = `Here are the regulars from  ${textToShowSame} who have been in ${textToShowOpposite} and the episodes they&apos;ve been in`;
+
   return (
     <>
+      <button onClick={() => setWhatToShow('doctorwho')}>Show Doctor Who regulars</button>
+      <button onClick={() => setWhatToShow('thebill')}>Show The Bill regulars</button>
+      {whatToShow && <h1>{headerText}</h1>}
+      {whatToShow && <p>{subtitleText}</p>}
       {doctorwho.map((person) => (
         <Card
           key={person.name}
