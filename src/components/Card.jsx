0ea501d8119083bceda5cc87c4c50ac159a129d3
“nicm42” <42087659+nicm42@users.noreply.github.com>
2023-12-25 18:32:19 +0000

    Put episodes into a modal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,46 @@
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import './Card.scss';
 
 function Card({ name, imgsrc, imgalt, episodes }) {
+  const [showEpisodes, setShowEpisodes] = useState();
+  const closeButtonRef = useRef(null);
+  let closeButtonHeight = 0;
+
+  useEffect(() => {
+    if (showEpisodes) {
+      closeButtonHeight = closeButtonRef.current?.offsetHeight;
+      document.documentElement.style.setProperty('--close-button-height', `${closeButtonHeight}px`);
+    }
+  }, [showEpisodes]);
+
   const imageURL = `assets/${imgsrc}`;
 
   return (
     <div className="card" data-testid="card">
-      <img className="card--image" src={imageURL} alt={imgalt} />
+      <img className="card--image" loading="lazy" src={imageURL} alt={imgalt} />
       <div className="card--text">
         <h2 className="card--name">{name}</h2>
-        <ul className="card--episodes">
-          {episodes.map((episode) => (
-            <li className="card--episode" key={episode}>
-              {episode}
-            </li>
-          ))}
-        </ul>
+        <button className="card--button" onClick={() => setShowEpisodes(true)}>
+          Show episodes
+        </button>
+        {showEpisodes && (
+          <>
+            <div className="card--episodes-overlay"></div>
+            <div className="card--episodes">
+              <button className="card--episodes-close" ref={closeButtonRef} onClick={() => setShowEpisodes(false)}>
+                X
+              </button>
+              <ul className="card--episodes-list">
+                {episodes.map((episode) => (
+                  <li className="card--episode" key={episode}>
+                    {episode}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
