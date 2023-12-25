288f7a99d9d75e00119290987dd1efe0ff177423
“nicm42” <42087659+nicm42@users.noreply.github.com>
2023-12-25 17:30:18 +0000

    Improved card layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,20 @@ import './Card.scss';
 
 function Card({ name, imgsrc, imgalt, episodes }) {
   const imageURL = `assets/${imgsrc}`;
-  // Put a comma between each episode
-  const formattedEpisodes = episodes.map((episode, index) => (index > 0 ? `, ${episode}` : episode));
 
   return (
     <div className="card" data-testid="card">
-      <h2 className="card--name">{name}</h2>
       <img className="card--image" src={imageURL} alt={imgalt} />
-      <p className="card--episodes">{formattedEpisodes}</p>
+      <div className="card--text">
+        <h2 className="card--name">{name}</h2>
+        <ul className="card--episodes">
+          {episodes.map((episode) => (
+            <li className="card--episode" key={episode}>
+              {episode}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
